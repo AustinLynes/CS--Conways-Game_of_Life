004237b7a2fcf7e3a5579b892f2046fee88ce4dc
= <=>
2020-06-17 23:16:10 -0700

    got grid synched with redux and now algorithm works game of life is now functional and now needs some style and presentational needs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Stage, Layer } from "react-konva"
-import { GameOfLife, toggleCellAlive } from './util/grid'
 import Cell from './components/shapes/cell'
 import { connect } from 'react-redux'
-import { createCells } from './store/actions';
+import { createCells, toggleCell, handleLife } from './store/actions';
 // GLOBALS
 const __SCREEN_WIDTH__ = 800
 const __SCREEN_HEIGHT__ = 800
@@ -13,12 +12,12 @@ const __GRID_ROWS__ = __SCREEN_HEIGHT__ / ___CELL_SIZE__
 const __GRID_COLS__ = __SCREEN_WIDTH__ / ___CELL_SIZE__
 
 
-const App = (props) => {
+const App = (store) => {
   // EDITOR SETTINGS
-  const { createCells, cells } = props
-  const [startSimulation, setStartSimulation] = useState(false)
+  const { cells, createCells, toggleCell, handleLife, is_toggling, generation, is_generating, population } = store
 
-  // const [cells, setCells] = useState(props.createCells(__GRID_COLS__, __GRID_ROWS__))
+  const [startSimulation, setStartSimulation] = useState(false)
+  // const [cells, setCells] = useState(store.cells)
 
   const [dimensions, setDimensions] = useState({
     width: __SCREEN_WIDTH__,
@@ -27,36 +26,61 @@ const App = (props) => {
     cols: __GRID_COLS__,
     rows: __GRID_ROWS__
   })
-  useEffect(() => {
-    createCells(__GRID_COLS__, __GRID_ROWS__)
-  }, [])
-  useEffect(() => {
-    if (startSimulation)
-      var loop = setInterval(() => {
-        // GameOfLife(cells, setCells)
-      }, 1000)
 
-    console.log('STOPPING')
-    return () => clearInterval(loop)
+
+  // useEffect(() => {
+  //   console.log("MOUNTED")
+  //   createCells(__GRID_COLS__, __GRID_ROWS__)
+  // }, [])
+
+  /**
+   *  SIMULATION LOOP, WILL END EITHER IF THE STOP BUTTON IS PRESSED OR IF THE GAME STATE REACHES GAME_OVER
+   */
+  useEffect(() => {
+    if (startSimulation) {
+
+      var simulate = setInterval(() => {
+        console.log("simulating")
+        if (population > 0) {
+          handleLife(cells)
+        } else {
+          setStartSimulation(false)
+          createCells(__GRID_COLS__, __GRID_ROWS__)
+        }
+      }, 0)
+    }
+    return () => clearInterval(simulate)
 
   }, [startSimulation])
+
+
+  useEffect(() => {
+    // setCells(store.cells)
+  }, [is_toggling, is_generating])
 
   const togglePlay = () => {
     setStartSimulation(!startSimulation)
   }
-  const setAlive = (e) => {
-    console.log("clicked ", e.target.attrs.id)
-    // toggleCellAlive(cells, e.target.attrs.id, setCells)
-    console.table(cells)
-  }
+
   return (
     <div className="App">
+
       <Stage className="game-view" width={dimensions.width} height={dimensions.height}>
-        <Layer>
+        <Layer >
           {
             cells.map(
-              (row, i) => row.map(
-                (cell, j) => <Cell id={`${i}-${j}`} key={`${i}-${j}`} x={i} y={j} setAlive={setAlive} alive={cell.alive} />,
+              (row, x) => row.map(
+                (cell, y) => <Cell
+                  cells={cells}
+                  id={`${cell.x}-${cell.y}`}
+                  key={`${cell.x}-${cell.y}`}
+                  x={cell.x}
+                  y={cell.y}
+                  toggleCell={toggleCell}
+                  isAlive={cell.isAlive}
+                  is_toggling={is_toggling}
+                  is_generating={is_generating}
+                  generation={generation} />,
               )
             )
           }
@@ -72,12 +96,18 @@ const App = (props) => {
 
 const __props = (store) => {
   return {
-    cells : store.cells
+    cells: store.cells,
+    is_toggling: store.is_toggling,
+    is_generating: store.is_generating,
+    generation: store.generation,
+    population: store.population
   }
 }
 
 const __actions_list = {
-  createCells
+  createCells,
+  toggleCell,
+  handleLife
 }
 
 export default connect(__props, __actions_list)(App);
